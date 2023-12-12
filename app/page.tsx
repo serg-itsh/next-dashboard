950dbbefa049431270d216e0b970a32c68bc842f
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2023-12-12 13:34:00 +0200

    change url in heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon } from '@heroicons/react/outline';
 import Link from '@/node_modules/next/link';
 // import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
