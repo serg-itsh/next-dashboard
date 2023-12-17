6b46137c1a53317960eb2e042249f4769c9986aa
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2023-12-17 18:31:39 +0200

    Added metadata

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -6,7 +6,8 @@ import {
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
 
-export default async function CustomersTable({
+// export default async function CustomersTable({
+  export default function CustomersTable({
   customers,
 }: {
   customers: FormattedCustomersTable[];
